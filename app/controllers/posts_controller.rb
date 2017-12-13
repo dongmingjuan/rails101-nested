70ff4ae10486e77f0f555f198337559a6f4e1317
dongmingjuan <https://github.com/dongmingjuan>
2017-12-13 16:51:29 +0800

    install simple_form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   def index
-
     @group = Group.find(params[:group_id])
     @posts = @group.posts
   end
@@ -8,6 +7,21 @@ class PostsController < ApplicationController
   def new
     @group = Group.find(params[:group_id])
     @post = @group.posts.new
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path(group_id: params[:group_id])
+    else
+      render :edit
+    end
   end
 
   def create
