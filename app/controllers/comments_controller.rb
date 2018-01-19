929867eaa58c5f91d8e30cd45d28a43e31b05f97
dongmingjuan <https://github.com/dongmingjuan>
2018-01-19 09:36:11 +0800

    finish the comment crud

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,6 +19,33 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to comments_path(post_id: params[:post_id])
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to comments_path(post_id: params[:post_id])
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:title)
